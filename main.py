e26e1a716f121f8d2b6b9fa0c93954a2b6ec2903
walei <126423501+Walei008@users.noreply.github.com>
2023-03-01 22:57:14 +0000

    there was an error on the last code

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 #Write your code below this row 👇
 
 for number in range (1,101):
-  if number%3:
+  if number%3==0:
     print(number)
     print("fizz")
-  if number%5:
+  if number%5==0:
     print(number)
     print("buzz")
-  if number%3%5:
+  if number%3==0 and number%5==0:
     print(number)
     print("fizzbuzz")
+  else:
+    print(number)
